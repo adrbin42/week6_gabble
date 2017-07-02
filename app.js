ed622bfd30e6caaf645abf1691ec19dd0bde873d
adrienne <adriennebing@gmail.com>
2017-07-01 21:22:13 -0400

    got signup working and basic forms working

--- app.js
+++ app.js
@@ -11,13 +11,15 @@ const app = express();
 app.engine('mustache', mustacheExpress());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'mustache');
-app.use('public', express.static('public'));
+// app.set('layout','layout');
 
+app.use('public', express.static('public'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(__dirname + '/public'));
 app.use(expressValidator());
 app.use(morgan('dev'));
 app.use(routes);
+
 
 app.listen(3200, function(){
   console.log('App running on http://localhost:3200');
--- routes/router.js
+++ routes/router.js
@@ -1,18 +1,61 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../models');
+const session = require('express-session');
+
+router.use(session({
+  secret:'winnerswin',
+  resave: false,
+  saveUninitialized: false
+}));
+// const message =
+// const like =
+// const gab =
 
 //Render app home page
 router.get('/', function(req,res){
-  res.render('index');
-});
+  // models.users.findOne().then(function(user){
+  //   console.log(user);
+  // })
 
-router.get('/newgab', function(req,res){
-  res.render('newgab');
+  if(req.session.user){
+    res.render("index",
+  {user:req.session.user});
+  }else{
+    res.redirect("/login");
+  }
 });
 
 router.get('/login', function(req,res){
   res.render('login');
+});
+
+router.post('/login', function(req,res){
+
+});
+
+router.get('/signup',function(req,res){
+  res.render('signup');
+});
+
+router.post('/signup',function(req,res,next){
+  const user = models.users.build({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    username: req.body.username,
+    password: req.body.password
+  });
+  
+  user.save().then(function(newUser){
+    console.log(newUser);
+  });
+
+  req.session.user = user;
+  res.render("sign up works","login");
+});
+
+router.get('/newgab', function(req,res){
+  res.render('newgab');
 });
 
 router.get('/likes', function(req,res){
